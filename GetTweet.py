12f8bd8a0f4d75feeba24f9a3bb3026ca62062c9
finyw <finyw@DESKTOP-NOMHP5D>
2020-04-25 12:10:23 +0200

    eve

--- GetTweet.py
+++ GetTweet.py
@@ -1,8 +1,14 @@
 import sys
 import tweepy
+import tkinter as tk
+
 #override tweepy.StreamListener to add logic to on_status
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
         print(status.text)
-#ISTHIS
+#ISTHISc
+
+win=tk.Tk()
+win.title("Get news")
+win.mainloop()
