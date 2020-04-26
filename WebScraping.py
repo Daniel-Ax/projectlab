60cbee7f13e33160e8776fb15eeccef7eb77be04
finyw <finyw@DESKTOP-NOMHP5D>
2020-04-26 12:31:33 +0200

    demoforpepeD

--- WebScraping.py
+++ WebScraping.py
@@ -1,7 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 
-source=requests.get('http://hvg.hu/').text
-soup= BeautifulSoup(source,'lxml')
-print(soup.prettify())
+#source=requests.get('http://hvg.hu/').text
+source=requests.get('http://coreyms.com/').text
+soup=BeautifulSoup(source,'lxml')
+for article in soup.find_all('article'):
+    headline=article.h2.a.text
+    print(headline)
+    summary=article.find('div', class_='entry-content').p.text
+    print(summary)
+    vid_src=article.find('iframe',class_='youtube-player')['src']
 
+    vid_id=vid_src.split('/')[4]
+    vid_id=vid_id.split('?')[0]
+    #print(vid_id)
+    yt_link=f'https://youtube.com/wtach?v={vid_id}'
+    print(yt_link)
+
+    print()
