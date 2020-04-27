7276d7c15ba71b29f76c6aaed45b0126f7e6fe23
Ács Dániel <finywow@gmail.com>
2020-04-27 11:12:01 +0200

    Pulled news from hvg

--- HVG.py
+++ HVG.py
@@ -6,3 +6,14 @@ source=requests.get('http://www.hvg.hu/').text
 soup=BeautifulSoup(source,'lxml')
 csv_file=open('hvg_scrape.csv','w')
 csv_writer=csv.writer(csv_file)
+
+fg=soup.find_all('div')
+for text in fg:
+    text1=text.h1
+    print(text1)
+    if text1==None:
+        csv_file.write("Empty text")
+    else:
+        csv_file.write(str(text1))
+
+
