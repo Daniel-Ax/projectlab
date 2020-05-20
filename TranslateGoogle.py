779efc4b4130c32fec0d9da02bfc4f43154aeba2
finyw <finyw@DESKTOP-NOMHP5D>
2020-05-20 19:32:29 +0200

    widepepohappy

--- TranslateGoogle.py
+++ TranslateGoogle.py
@@ -17,3 +17,7 @@ for line in mytext:
     print(f'Source:{t.src}')
     print(f'Destination:{t.dest}')
     print(f'{t.origin} -> {t.text}')
+    print(type(t.text))
+file_to_write=open('destination','w')
+file_to_write.write(str(t.text))
+file_to_write.close()
