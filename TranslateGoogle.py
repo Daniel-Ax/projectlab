e68592dd09b84fdb3d888ed7eacf0c01dbb6388c
finyw <finyw@DESKTOP-NOMHP5D>
2020-05-20 18:38:25 +0200

    -added translator fix2.0

--- TranslateGoogle.py
+++ TranslateGoogle.py
@@ -1,1 +1,9 @@
 from googletrans import Translator
+
+trans=Translator()
+t=trans.translate(
+    'Bom dia',src='pt', dest='hu'
+)
+print(f'Source:{t.src}')
+print(f'Destination:{t.dest}')
+print(f'{t.origin} -> {t.text}')
